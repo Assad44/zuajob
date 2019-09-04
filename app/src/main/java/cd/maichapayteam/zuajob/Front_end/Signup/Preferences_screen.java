0ced2af5ab0cf77cb445ac5ec3e2f94b14a22acb
elikyalk <lkdslai@gmail.com>
2019-09-04 17:45:03 +0100

    Modification de l'activité préférence : ajout de la partie logic

--- app/src/main/java/cd/maichapayteam/zuajob/Front_end/Signup/Preferences_screen.java
+++ app/src/main/java/cd/maichapayteam/zuajob/Front_end/Signup/Preferences_screen.java
@@ -2,8 +2,10 @@ package cd.maichapayteam.zuajob.Front_end.Signup;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
@@ -11,11 +13,19 @@ import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.zip.DataFormatException;
 
 import cd.maichapayteam.zuajob.Front_end.Home;
+import cd.maichapayteam.zuajob.Models.Object.User;
 import cd.maichapayteam.zuajob.R;
+import cd.maichapayteam.zuajob.Tools.RemoteDataSync;
+import cd.maichapayteam.zuajob.Tools.Tool;
 
 public class Preferences_screen extends AppCompatActivity {
 
@@ -33,6 +43,7 @@ public class Preferences_screen extends AppCompatActivity {
 
 
     private void Init_Components(){
+        //TODO si l'utilisateur a moins de 18 ans n'afficher pas la caégorie sexe
         btn_back_arrow = findViewById(R.id.btn_back_arrow);
         btn_next = findViewById(R.id.btn_next);
         list_item = findViewById(R.id.list_item);
@@ -56,7 +67,7 @@ public class Preferences_screen extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
 
-        // Todo ; Initialisation des composants
+        // Initialisation des composants
         Init_Components();
     }
 
@@ -107,9 +118,8 @@ public class Preferences_screen extends AppCompatActivity {
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(context, Home.class);
-                startActivity(i);
-                finish();
+                CreationAccountAsync creationAccountAsync = new CreationAccountAsync();
+                creationAccountAsync.execute();
             }
         });
     }
@@ -119,6 +129,74 @@ public class Preferences_screen extends AppCompatActivity {
         Intent i = new Intent(context, Password_screen.class);
         startActivity(i);
         finish();
+    }
+
+    class CreationAccountAsync extends AsyncTask<String, String, User> {
+
+        @Override
+        protected void onPreExecute() {
+            //TODO : show a load dialog here
+            super.onPreExecute();
+        }
+
+        @Override
+        protected User doInBackground(String... strings) {
+            String urlPhoto = "";
+            String picture = Tool.getUserPreferences(Preferences_screen.this, "picture");
+            if(!picture.equals("default")) {
+                //TODO load image in server
+
+
+            }
+
+            User user = new User();
+            user.urlPhoto = urlPhoto;
+            //TODO ajouter le champs ville sur la liste
+            user.ville = "";
+            user.adresse = Tool.getUserPreferences(Preferences_screen.this, "avenue");
+            user.quartier = Tool.getUserPreferences(Preferences_screen.this, "quartier");
+            user.commune = Tool.getUserPreferences(Preferences_screen.this, "commune");
+            user.about = Tool.getUserPreferences(Preferences_screen.this, "apropos");
+            user.email = Tool.getUserPreferences(Preferences_screen.this, "email");
+            user.nom = Tool.getUserPreferences(Preferences_screen.this, "nom");
+            user.prenom = Tool.getUserPreferences(Preferences_screen.this, "prenom");
+            long birthday = 0;
+            try {
+                Date date = new SimpleDateFormat("").parse(Tool.getUserPreferences(Preferences_screen.this, "birthday"));
+                birthday = date.getTime();
+            } catch (ParseException e) {
+
+            }
+            user.birthday = birthday;
+            user.sexe = Tool.getUserPreferences(Preferences_screen.this, "sexe");
+            user.password = Tool.getUserPreferences(Preferences_screen.this, "passe");
+            user.phone = Integer.parseInt(Tool.getUserPreferences(Preferences_screen.this, "phone"));
+            user.codePays = Tool.getUserPreferences(Preferences_screen.this, "CountryCode");
+            user.pays = Tool.getUserPreferences(Preferences_screen.this, "CountryName");
+            user.type = Integer.parseInt(Tool.getUserPreferences(Preferences_screen.this, "type_user"));
+
+
+            //TODO see how to send preferrences in server
+
+            return RemoteDataSync.createUser(user);
+        }
+
+        @Override
+        protected void onPostExecute(User user) {
+            //TODO : dismiss a load dialog here
+            if(user.error) {
+                //TODO il y a erreur d'ajout, le signaler à l'utilisateur
+                Log.e("CreateUser", user.errorMessage);
+                Toast.makeText(Preferences_screen.this, user.errorMessage, Toast.LENGTH_LONG).show();
+            } else {
+                //Le user s'est incrit avec succès et ses informations sont enregistrées dans la base de données
+                Intent i = new Intent(context, Home.class);
+                startActivity(i);
+                finish();
+            }
+
+            super.onPostExecute(user);
+        }
     }
 
 }
