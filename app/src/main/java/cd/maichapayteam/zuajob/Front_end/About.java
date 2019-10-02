edab481ea6c133792356f1acced351ace65ff8f2
Assad44 <massadig000@gmail.com>
2019-10-02 20:24:32 +0200

    up D3on M@ss

---
+++ app/src/main/java/cd/maichapayteam/zuajob/Front_end/About.java
@@ -0,0 +1,25 @@
+package cd.maichapayteam.zuajob.Front_end;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cd.maichapayteam.zuajob.Home;
+import cd.maichapayteam.zuajob.R;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        getSupportActionBar().hide();
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(this, Paramettres.class));
+        finish();
+    }
+
+}
